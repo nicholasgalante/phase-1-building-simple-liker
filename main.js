3e3f606ba08e830e15fc8763c6417eb39218fde4
Nicholas <nicholasgalante94@gmail.com>
2022-11-30 17:15:58 -0500

    commit tree dd48c99553c36ce0636eabd73686af0505086b5a

--- main.js
+++ main.js
@@ -4,16 +4,43 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const allHearts = document.querySelectorAll('.like-glyph');
 
+allHearts.forEach(element => {
+  element.addEventListener('click', (event) => {
+    mimicServerCall()
+      .then(() => {
+        updateHeart(event);
+      })
+      .catch(() => {
+        const modal = document.querySelector('#modal');
+        const modalMessage = document.querySelector('#modal-message');
+        modal.classList.remove('hidden');
+        modalMessage.innerText = "Random server error. Try again."
+        setTimeout(() => {
+          modal.classList.add('hidden');
+        }, 3000)
+      });
+  })
+});
 
+updateHeart = (event) => {
+  if (event.target.classList.contains('activated-heart')){
+    event.target.innerText = EMPTY_HEART;
+    event.target.classList.remove('activated-heart');
+  } else {
+    event.target.innerText = FULL_HEART;
+    event.target.classList.add('activated-heart');
+  }
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
@@ -21,5 +48,5 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
         resolve("Pretend remote server notified of action!");
       }
     }, 300);
-  });
+  })
 }
